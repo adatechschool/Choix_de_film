003e73eee0c93bc34da1c4260739482f18d81692
Wara-nyu <w.bounkeua@gmail.com>
2019-11-04 18:20:53 +0100

    ajouter randomisation de la list

--- choix_de_film.rb
+++ choix_de_film.rb
@@ -1,3 +1,27 @@
-#.length -> cout amout of rows
-#filmx1,filmx2,filmx3
-#filxy1,filmy2,filmy3
+require 'csv'
+
+table = CSV.read("a_voir.csv")
+
+x = table.length
+x -= 1
+y = rand(0..x)
+puts table[y][0]
+table[y].delete_at(0)
+
+if table[y].length == 0
+	table.delete_at(y)
+end
+y = 0
+table.each do |item|
+	table[y].each do |x|
+		print "#{x}"
+	end
+	puts ""
+	y += 1
+end
+
+CSV.open("a_voir.csv", "w") do |csv|
+	table.each do |item| 
+		csv << item
+	end
+end
