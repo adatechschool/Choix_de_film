8f10b8f8ce82d271a4b232e9395d3414408d15c3
Nydragon00 <6582485n@gmail.com>
2019-11-05 15:04:49 +0100

    test

---
+++ choix_de_film.rb
@@ -0,0 +1,27 @@
+require 'csv'
+
+table = CSV.read("a_voir.csv")
+
+x = table.length
+x -= 1
+y = rand(0..x)
+puts table[y][0]
+table[y].delete_at(0)
+
+if table[y].length == 0
+	table.delete_at(y)
+end
+y = 0
+table.each do |item|
+	table[y].each do |x|
+		print "#{x}"
+	end
+	puts ""
+	y += 1
+end
+
+CSV.open("a_voir.csv", "w")  do |csv|
+  table.each do |item|
+    csv << item
+  end
+end
