1460ad4b66a7c9758b1e88b6736e56ff5315fa53
Nydragon00 <6582485n@gmail.com>
2019-11-06 15:13:51 +0100

    Improvements

--- TestChoix.rb
+++ TestChoix.rb
@@ -1,24 +1,48 @@
 require 'csv'
 require 'colorize'
 
-avoir = "#{File.expand_path File.dirname(__FILE__)}/a_voir.csv"
+avoir = "#{File.expand_path File.dirname(__FILE__)}/movies.csv"
 vu = "#{File.expand_path File.dirname(__FILE__)}/vu.csv"
 table = CSV.read(avoir)
+buffer = CSV.read(avoir)
 
 x = table.length
 x -= 1
-y = rand(0..x)
-movie = table [y][0]
 
-puts "Vous-voulez voir " + movie + " ?"
-answer = gets.strip!
-if answer == "oui"
-	puts "Oh yeah!!! ( ͡° ͜ʖ ͡°)".green
-	table[y].delete_at(0)
-	if table[y].length == 0
-		table.delete_at(y)
+
+
+while true
+	y = rand(0..x)
+	if y = 0
+		y = 1
 	end
+	movie = buffer [y]
+	print "Vous-voulez voir \"#{movie[0]}\"? y/n: "
+	answer = gets.strip!
+
+	if answer == "y"
+		puts "Oh yeah!!! ( ͡° ͜ʖ ͡°)".green
+		CSV.open(vu, "a") do |csv|
+			csv << movie
+		end
+		table.delete_at(y)
+		CSV.open(avoir, "w") do |csv|
+			table.each do |item|
+				csv << item
+			end
+		end
+		break
+	elsif answer == "n"
+		puts "Poooouuuurrrqqquuuooooiiiiiiiii !!! (/;o;)/".yellow
+		buffer.delete_at(y)
+	end
+end
+
+
 =begin
+else
+puts "(°_o) rage quit => []".red
+end
 	y = 0
 	table.each do |item|
 		table[y].each do |x|
@@ -30,19 +54,4 @@ if answer == "oui"
 =end
 
 =begin
-	CSV.open(avoir, "w") do |csv|
-		table.each do |item|
-			csv << item
-		end
-	end
-
-	CSV.open(vu, "a") do |csv|
-		csv << [movie]
-	end
 =end
-elsif answer == "non"
-	puts "Poooouuuurrrqqquuuooooiiiiiiiii !!! (/;o;)/".yellow
-else
-	puts "(°_o) rage quit => []".red
-end
-exit
