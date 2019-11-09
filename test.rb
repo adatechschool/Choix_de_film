caff502b7bad7d2d7d91bafe471d0bfedaf46ba7
Nydragon00 <56591727+Nydragon00@users.noreply.github.com>
2019-11-09 23:31:18 +0100

    Hello There
    General Kenobi

--- test.rb
+++ test.rb
@@ -92,12 +92,74 @@ class Calc_stats
 end
 
 class Sort_stats
+
+  def sort_studio(studio, csv, sort_type)
+    std_array = []
+    studio_array = []
+    i = 0
+
+    csv.each do |x|
+      csv[i] = Movies.new(csv[i][0..-1])
+      std_array << csv[i].hash()
+      i += 1
+    end
+
+    std_array.each do |x|
+      if x["Lead Studio"] == studio
+        studio_array << x
+      end
+    end
+
+    header = "0--".ljust(5) + "Genre".ljust(15) + "Film".ljust(40) + "Audience Score in %".ljust("Audience Score in %".length + 7) + "Profitability in %".ljust("Profitability in %".length + 5) + "Rotten Tomatoes in %" + "\n"
+    puts header
+    puts "-"*header.length
+    counter = 1
+    studio_array.sort_by!{ |x| x[sort_type]}.reverse!
+    studio_array.each do |x|
+      if counter < 10
+        puts "#{counter}---".ljust(5) + x["Genre"].ljust(15) + x["Film"].ljust(40) + "#{x["Audience Score"].to_i}" + "%".ljust("Audience Score in %".length + 5) + "#{x["Profitability"].to_f.round(3)}%".ljust("Profitability in %".length + 5) + "#{x["Rotten Tomatoes"].to_i}%" + "#{x["Worldwide Gross"].to_f.round(3)}." + x["Year"]
+        counter += 1
+      elsif counter > 9
+        puts "#{counter}--".ljust(5) + x["Genre"].ljust(15) + x["Film"].ljust(40) + "#{x["Audience Score"].to_i}" + "%".ljust("Audience Score in %".length + 5) + "#{x["Profitability"].to_f.round(3)}%".ljust("Profitability in %".length + 5) + "#{x["Rotten Tomatoes"].to_i}%" + "#{x["Worldwide Gross"].to_f.round(3)}" + x["Year"]
+        counter += 1
+      end
+    end
+  end
+
+  def sort_all(sort_type, asc_or_desc)
+    #Sortierungen die auf alle Studios angewendet werden
+  end
 end
 
+sorting = Sort_stats.new
+
+while true
+  print "Do you want to inspect [1] everything or only a [2] specific studio: "
+  scope = gets.chomp.to_i
+  case scope
+  when 2
+    print "Enter the studio you want to inspect: "
+    studio = gets.chomp
+    print "Enter the characteristic you want to sort: "
+    sort = gets.chomp
+    puts ""
+    sorting.sort_studio(studio, csv, sort)
+  end
+  print "\nIf you want to exit enter [x]: "
+  exit = gets.chomp
+  if exit == "x"
+    break
+  end
+end
+
+
+
+
+=begin
 i = 0
 std_array = []
-stats_array = []
 studio_array = []
+stats_array = []
 
 #create array of hashes from the csv file
 csv.each do |x|
@@ -127,4 +189,5 @@ puts std_array
 #array der stats pro Studio, um Durchschnitt >> stats/movie_count
 puts stats_array
 #array der Studios
-puts studio_array
+#puts studio_array
+=end
