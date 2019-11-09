e9f60293e21f42e391335a61e73e3600d4c96984
Nydragon00 <56591727+Nydragon00@users.noreply.github.com>
2019-11-09 19:07:10 +0100

    WIR KOMMEN DEM ENDZIEL NÄHER

--- test.rb
+++ test.rb
@@ -1,5 +1,7 @@
 require 'csv'
 
+#csv header: Film,Genre,Lead Studio,Audience score %,Profitability,Rotten Tomatoes %,Worldwide Gross,Year
+
 film_csv = "#{File.expand_path File.dirname(__FILE__)}/movies.csv"
 csv = CSV.read(film_csv)
 
@@ -10,35 +12,119 @@ class Movies
     @i = i
   end
   def hash()
-    return {:"Film" => i[0],
-            :"Genre" => i[1],
-            :"Lead Studio" => i[2],
-            :"Audience Score" => i[3].to_i,
-            :"Profitability" => i[4].to_i,
-            :"Rotten Tomatoes" => i[5].to_i,
-            :"Worldwide Gross" => i[6].to_i,
-            :"Year" => i[7].to_i}
-  end
-  def calculate_stats()
-
+    return {"Film" => i[0],
+            "Genre" => i[1],
+            "Lead Studio" => i[2],
+            "Audience Score" => i[3],
+            "Profitability" => i[4],
+            "Rotten Tomatoes" => i[5],
+            "Worldwide Gross" => i[6][1..],
+            "Year" => i[7]}
   end
  end
 
+class Calc_stats
+  :studio_counter
+
+  def initialize(hash_array, studio)
+    @hash_array = hash_array
+    @studio = studio
+  end
+
+  def audience_sc()
+    counter = 0
+    @hash_array.each do |x|
+      if x["Lead Studio"] == @studio
+        counter += x["Audience Score"].to_i
+      end
+    end
+    return counter
+  end
+
+  def profitability()
+    counter = 0
+    @hash_array.each do |x|
+      if x["Lead Studio"] == @studio
+        counter += x["Profitability"].to_f
+      end
+    end
+    return counter
+  end
+
+  def tomatoes_sc()
+    counter = 0
+    @hash_array.each do |x|
+      if x["Lead Studio"] == @studio
+        counter += x["Rotten Tomatoes"].to_i
+      end
+    end
+    return counter
+  end
+
+  def tt_ww_gross()
+    counter = 0
+    @hash_array.each do |x|
+      if x["Lead Studio"] == @studio
+        counter += x["Worldwide Gross"].to_f
+      end
+    end
+    return counter
+  end
+
+  def movie_counter()
+    counter = 0
+    @hash_array.each do |x|
+      if x["Lead Studio"] == @studio
+        counter += 1
+      end
+    end
+    return counter
+  end
+
+  def return_stats()
+    return studio_stats = {"Studio Name" => @studio,
+                           "Audience Score" => audience_sc(),
+                           "Profitability" => profitability(),
+                           "Tomatoes Score" => tomatoes_sc(),
+                           "Total Gross" => tt_ww_gross(),
+                           "Movie Amount" => movie_counter()}
+  end
+end
+
+class Sort_stats
+end
 
 i = 0
-hash = []
+std_array = []
+stats_array = []
+studio_array = []
+
+#create array of hashes from the csv file
 csv.each do |x|
   csv[i] = Movies.new(csv[i][0..-1])
-  hash << csv[i].hash()
-=begin
-  if i == 0
-    binded = csv[i].get_binded(csv[i][0..2])
-  end
-=end
-  #print "#{csv[i].i[0]}\n"
+  std_array << csv[i].hash()
   i += 1
 end
 
-if hash { |h| h[:"Lead Studio"] == 'Disney' }
-  puts hash.fetch("Worldwide Gross")
+
+
+
+# array mit allen Studionamen
+std_array.each do |x|
+  if studio_array.include?(x.fetch("Lead Studio")) == false
+    studio_array.push(x.fetch("Lead Studio"))
+  end
 end
+
+studio_array.each do |x|
+  prof = Calc_stats.new(std_array, x)
+  stats_array << prof.return_stats()
+end
+
+
+#array von allen Filmen
+puts std_array
+#array der stats pro Studio, um Durchschnitt >> stats/movie_count
+puts stats_array
+#array der Studios
+puts studio_array
